b7c9136cd84a90a597a906c032cfd01ea95f03d8
julio olivares <julioolivares90@gmail.com>
2017-05-10 19:46:17 -0600

    ejemplo de login con base mysql web services

---
+++ app/src/main/java/com/example/julio/loginbd/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.julio.loginbd;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import registro.RegisterUserClasss;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText txtusuario,txtpass;
+    private Button btnlogin;
+
+    public static final String USER_NAME="USER_NAME";
+    public static final String PASSWORD="PASSWORD";
+    private static final String LOGIN_URL = "http://localhost/rutasUtec/login.php";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtusuario = (EditText)findViewById(R.id.txtusuario);
+        txtpass=(EditText)findViewById(R.id.txtpass);
+        btnlogin=(Button)findViewById(R.id.btnlogin);
+        btnlogin.setOnClickListener(this);
+    }
+    private void login(){
+        String username=txtusuario.getText().toString().trim();
+        String password= txtpass.getText().toString().trim();
+        //aqui va el metodo de login
+        userLogin(username,password);
+    }
+    private void userLogin(final String username,final String password){
+        class UserLoginClass extends AsyncTask<String,Void,String>{
+            ProgressDialog loading;
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading=ProgressDialog.show(MainActivity.this,"espere porfavor",null,true,true);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                if (s.equalsIgnoreCase("success")){
+                    Intent intent = new Intent(getApplicationContext(),Menu.class);
+                    intent.putExtra(USER_NAME,username);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(MainActivity.this,s,Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+                HashMap<String,String>data = new HashMap<>();
+                data.put("username",params[0]);
+                data.put("password",params[1]);
+                RegisterUserClasss registerUserClasss = new RegisterUserClasss();
+                String result = registerUserClasss.sedPostRequest(LOGIN_URL,data);
+
+                return result;
+            }
+        }
+        UserLoginClass userLoginClass = new UserLoginClass();
+        userLoginClass.execute(username,password);
+    }
+    @Override
+    public void onClick(View v) {
+        if(v ==btnlogin){
+            login();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/julio/loginbd/Registro.java
@@ -0,0 +1,96 @@
+package com.example.julio.loginbd;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.HashMap;
+
+import registro.RegisterUserClasss;
+
+public class Registro extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText txtnombre,txtusuarioR,txtemailR,txtpasswordR;
+    private Button btnregistro,btncancelarR;
+
+    private static final String REGISTER_URL="http://localhost/rutasUtec/registro.php";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+
+        txtnombre = (EditText)findViewById(R.id.txtnombre);
+        txtusuarioR = (EditText)findViewById(R.id.txtusuarioR);
+        txtemailR = (EditText)findViewById(R.id.txtemailR);
+        txtpasswordR=(EditText)findViewById(R.id.txtpassR);
+
+        btnregistro = (Button)findViewById(R.id.btnregistro);
+        btncancelarR=(Button)findViewById(R.id.btncancelarR);
+
+        btnregistro.setOnClickListener(this);
+        btncancelarR.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnregistro:
+                registrarusuario();
+                break;
+            case R.id.btncancelarR:
+                Intent intent =  new  Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+    private void registrarusuario(){
+        String name = txtnombre.getText().toString().trim().toLowerCase();
+        String username =txtusuarioR.getText().toString().trim().toLowerCase();
+        String password = txtpasswordR.getText().toString().trim().toLowerCase();
+        String email = txtemailR.getText().toString().trim().toLowerCase();
+
+        registro(name,username,password,email);
+    }
+    private void registro(String name,String username,String password,String email){
+        class RegisterUser extends AsyncTask<String,Void,String>{
+            ProgressDialog loading;
+            RegisterUserClasss registerUserClasss = new RegisterUserClasss();
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading=ProgressDialog.show(Registro.this,"espera porfavor",null,true,true);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+                HashMap<String,String>data = new HashMap<String, String>();
+                data.put("name",params[0]);
+                data.put("username",params[1]);
+                data.put("password",params[2]);
+                data.put("email",params[3]);
+                String result = registerUserClasss.sedPostRequest(REGISTER_URL,data);
+                return result;
+            }
+        }
+        RegisterUser ru  = new RegisterUser();
+        ru.execute(name,username,password,email);
+    }
+}
