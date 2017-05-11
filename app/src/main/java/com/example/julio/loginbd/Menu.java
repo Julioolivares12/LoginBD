c6cdd14ae7a59c9063b4b294151a58aa3bf87d20
julio olivares <julioolivares90@gmail.com>
2017-05-10 20:04:12 -0600

    agregue los permisos y un mensaje cuando pasa la pantalla

--- app/src/main/java/com/example/julio/loginbd/Menu.java
+++ app/src/main/java/com/example/julio/loginbd/Menu.java
@@ -1,13 +1,23 @@
 package com.example.julio.loginbd;
 
+import android.content.Intent;
+import android.support.v4.text.TextDirectionHeuristicCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Menu extends AppCompatActivity {
 
+    private TextView txtmensaje;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        txtmensaje=(TextView)findViewById(R.id.txtmensaje);
+        Intent intent = getIntent();
+        String username=intent.getStringExtra(MainActivity.USER_NAME);
+
+        txtmensaje.setText("Bienvenido usuario" +username);
     }
 }
